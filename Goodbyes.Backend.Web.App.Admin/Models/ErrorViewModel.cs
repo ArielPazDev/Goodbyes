765967d50ed4a8649166a1dc352de232932fa69b
ArielPazDev <contacto.arielpaz.dev@gmail.com>
2024-06-20 11:45:22 -0300

    NEW (Goodbyes.Backend.Web.App.Admin): Add new solution

---
+++ Goodbyes.Backend.Web.App.Admin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Goodbyes.Backend.Web.App.Admin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
