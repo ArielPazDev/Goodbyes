edd1707b2fd361b6c7a1b2a232fda98682aabec4
ArielPazDev <contacto.arielpaz.dev@gmail.com>
2024-06-24 07:20:00 -0300

    refactor (Solution): Rename projects

--- Goodbyes.Backend.Web.App.Admin/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Goodbyes.Backend.Web.App.Admin.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- Goodbyes.Backend.Web.App.User/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Goodbyes.Backend.Web.App.User.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
